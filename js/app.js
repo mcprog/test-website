c52d21a816b946102b76dc7f42216acdefd06947
mcprog <michaelcurtis96@gmail.com>
2015-01-27 22:44:46 -0800

    initial commit

---
+++ js/app.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+	//var inAninmation = 7;
+	$("#btn-curse").click(function(){
+			var curse = $('#curse-counter');
+			curse.animate({
+				top: '-=20px',
+				opacity: '0'
+			}, 'fast');
+			curse.animate({
+				top: '+=40px'
+			}, 0, function() {
+				var count = parseInt($('#curse-counter').text());
+				curse.text(count + 1);
+			});
+			curse.animate({
+				top: '-=20px',
+				opacity: '1'
+			}, 'fast');
+		//}
+	});
+});
